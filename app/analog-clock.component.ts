cd85fff02dfa7cc73f2280b4c38419cee76fb2db
Chad Schwendiman <Chad Schwendiman>
2016-10-11 00:19:40 -0700

    working analog clock

--- app/analog-clock.component.ts
+++ app/analog-clock.component.ts
@@ -1,18 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'analog-clock',
     template: `
-        <div id="seconds">
-            <img src="seconds.png">
+        <div style="position:absolute">
+            <img id="second" src="second.png">
+            <img id="minute" src="minute.png">
+            <img id="hour" src="hour.png">
         </div>
     `,
     styles: [
         `
-        
+        #second {
+            position: absolute;
+        }
+        #minute {
+            position: absolute;
+        }
+        #hour {
+            position: absolute;
+        }
         `
     ]
 })
-export class AnalogClockComponent {
+export class AnalogClockComponent implements OnInit {
+    private _secondImgElement: HTMLImageElement;
+    private _minuteImgElement: HTMLImageElement;
+    private _hourImgElement: HTMLImageElement;
 
+    ngOnInit() {
+        this._secondImgElement = <HTMLImageElement>document.getElementById("second");
+        this._secondImgElement.style.transformOrigin = "50% 50%";
+
+        this._minuteImgElement = <HTMLImageElement>document.getElementById("minute");
+        this._secondImgElement.style.transformOrigin = "50% 50%";
+
+        this._hourImgElement = <HTMLImageElement>document.getElementById("hour");
+        this._hourImgElement.style.transformOrigin = "50% 50%";
+
+        setInterval(() => { this.OnInterval();}, 1000);
+
+        //this._secondImgElement.style.transform = this.BuildRotateString(45);
+    }
+
+    OnInterval() {
+        var now = new Date(Date.now());
+        var milliseconds = now.getMilliseconds();
+        var seconds = now.getSeconds();
+        var minutes = now.getMinutes();
+        var hours = now.getHours();
+
+        var secondDegress = ((seconds * 1000) + milliseconds) * (6/1000);
+
+        var minuteDegrees = ((minutes * 60) + seconds) * (6/60);
+        
+        var hourDegrees = hours * 30;
+        hourDegrees += ((minutes * 60) + seconds) * (30/3600);
+
+        console.log(minuteDegrees);
+
+        this._secondImgElement.style.transform = this.BuildRotateString(secondDegress);
+        this._minuteImgElement.style.transform = this.BuildRotateString(minuteDegrees);
+        this._hourImgElement.style.transform = this.BuildRotateString(hourDegrees);
+
+    }
+
+    BuildRotateString(degrees: number): string {
+        return "rotate("+degrees+"deg)";
+    }
 }
